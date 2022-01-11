d1f5cc43cb86a240793579ea0f34dba23e8d78c0
Will <willchesson@gmail.com>
2022-01-11 17:45:46 -0500

    commit tree 5e0a7361cef704269e6729af8f5bd55057dd3f23

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(manzana) {
+    return Math.abs(manzana - 42);
+}
+
+function distanceFromHqInFeet(manzana) {
+    return distanceFromHqInBlocks(manzana) * 264;
+}
+
+function distanceTravelledInFeet(inicio, destino) {
+    return (Math.abs(inicio - destino)*264);
+}
+
+
+function calculatesFarePrice(inicio, destino) {
+    const distance = distanceTravelledInFeet(inicio, destino); 
+    
+    if (distance <= 400) {
+        return 0.00
+    }
+    else if (400 < distance && distance <= 2000) {
+        return (distance - 400) * 0.02
+    }
+    else if (2000 < distance && distance < 2500) {
+        return 25.00
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
